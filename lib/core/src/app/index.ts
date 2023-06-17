cb4a16bf0604d0de1ee9a317062c5c5e97528de0
Robby Emmert <robby@emmert.io>
2023-06-17 17:11:51 -0400

    workspaces importing working

---
+++ lib/core/src/app/index.ts
@@ -0,0 +1,17 @@
+export type AppMeta = {
+  /**
+   * A human-friendly title for your app
+   */
+  title: string;
+  /**
+   * A unique parameterized name for your app
+   */
+  name: string;
+  /**
+   * A human-friendly description for your app
+   */
+  description: string;
+  /**
+   * A URL to a logo for your app
+   */
+};
--- lib/core/src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from "vitest";
-import { test } from ".";
-
-describe("index", () => {
-  it("Should export stuff", ({ expect }) => {
-    expect(test).toBe("Hello World!");
-  });
-});

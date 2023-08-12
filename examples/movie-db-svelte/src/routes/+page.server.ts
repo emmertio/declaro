803e09a93d6235d293b573fd77a0b686624c1ea7
Robby Emmert <robby@emmert.io>
2023-08-12 12:57:20 -0400

    Adding example of using models on the front end

--- examples/movie-db-svelte/src/routes/+page.server.ts
+++ examples/movie-db-svelte/src/routes/+page.server.ts
@@ -1,7 +1,15 @@
-import { test } from '@declaro/core';
+import { Movie } from '$models/Movie';
 
 export async function load() {
+	const movie = new Movie();
+	movie.title = 'The Matrix';
+	movie.year = 1999;
+	movie.description =
+		'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.';
+
 	return {
-		test
+		movie: {
+			...movie
+		}
 	};
 }
--- lib/build/src/filesystem/file-manager.ts
+++ lib/build/src/filesystem/file-manager.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import { resolve } from 'path'
+import { createFilter, type FilterPattern } from 'vite'
 import { type PluginConfig } from '../config/plugin-config'
-import { type FilterPattern, createFilter } from 'vite'
 
 export class FileManager {
     static inject = ['PluginConfig'] as const

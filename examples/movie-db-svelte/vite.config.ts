0b3cb040d7abd0587b8f53d5d3d8f3a8ab8a7a96
Robby Emmert <robby@emmert.io>
2023-08-22 14:00:17 -0400

    Implementing interface generator

--- examples/movie-db-svelte/vite.config.ts
+++ examples/movie-db-svelte/vite.config.ts
@@ -1,12 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { ClassModelGenerator, ReferenceGenerator, declaro } from '@declaro/build';
+import {
+	ClassModelGenerator,
+	ReferenceGenerator,
+	InterfaceModelGenerator,
+	declaro
+} from '@declaro/build';
 
 export default defineConfig({
 	plugins: [
 		declaro({
 			models: {
-				generators: [new ClassModelGenerator(), new ReferenceGenerator()]
+				generators: [
+					new ClassModelGenerator(),
+					new ReferenceGenerator(),
+					new InterfaceModelGenerator()
+				]
 			}
 		}),
 		sveltekit()

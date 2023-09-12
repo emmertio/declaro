0c676c0cd4fb861dcca478e52f81c0ebfff69b5b
Michael Ambrose <paco3346@gmail.com>
2023-09-12 15:39:23 -0400

    Updated datastore abstract upsert method so that it actually works
    This was only tested on non-nested objects before which is why it didn't work
    Entities are now shallow copied and persisted using the proper Mikro methods

--- lib/data/src/databaseConnection.ts
+++ lib/data/src/databaseConnection.ts
@@ -2,6 +2,7 @@ import type { IDatastoreProvider, BaseModel, BaseModelClass } from '@declaro/cor
 import type { EntityManager, FilterQuery, Reference } from "@mikro-orm/core";
 import type { EntityRepository } from '@mikro-orm/postgresql'
 import { Hydrator } from "./hydrateEntity";
+import { v4 as uuidv4 } from 'uuid';
 
 
 export class DatabaseConnection<T extends BaseModel<any>> implements IDatastoreProvider<T> {
@@ -42,21 +43,37 @@ export class DatabaseConnection<T extends BaseModel<any>> implements IDatastoreP
         })
     }
 
-    upsert<T extends BaseModel<any>>(data: T) {
-        let p;
-        const entity = this.hydrator.hydrateEntity<T>(data);
+    async upsert<T extends BaseModel<any>>(data: T) {
+        let entity: T;
 
-        if (typeof entity.id === 'undefined') {
-            p = this.em.insert(entity).then(id => {
-                return this.repository.findOne(id);
-            });
-        } else {
-            p = this.repository.upsert(entity);
-        }
-        return p.then((o: T) => {
-            return this.em.flush().then(() => {
-                return o;
-            });
+        // Get entity metadata
+        const meta = this.em.getMetadata().get(data.constructor.name);
+
+        // Create a shallow copy of data, excluding m:n fields
+        const shallowData: Partial<T> = {};
+        Object.keys(data).forEach((key) => {
+            const property = meta.properties[key];
+            if (!property || property.reference !== 'm:n') {
+                shallowData[key] = data[key];
+            }
         });
+
+        if (!data.id) {
+            shallowData.id = data.id = uuidv4();
+            // Create new entity with shallow data
+            entity = this.em.create(data.constructor.name, shallowData as any);
+            await this.em.persist(entity).flush();
+        } else {
+            // Fetch and merge for existing entity
+            entity = await this.em.findOneOrFail(data.constructor.name, data.id);
+        }
+
+        // Use em.assign to properly handle m:n relationships for both new and existing entities
+        this.em.assign(entity, data);
+
+        await this.em.persist(entity).flush();
+
+        return entity;
     }
+
 }

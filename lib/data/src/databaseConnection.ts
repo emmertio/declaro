10bfaeb33d70d77cadd17959176e6353ea52d17e
Michael Ambrose <paco3346@gmail.com>
2023-10-24 17:07:57 -0400

    Fixed bug where newly created entities could not have many to many relations created during initial insert
    Many to many realtionships use a linking table. In order to use this the ID for both linked entities must already exist. That's the purpose of the shallow clone here (and why m:n fields are ignored). The problem was that the old code never called em.assign after that insert to actually put in the relationship. (Updates did, but not inserts). Now, both inserts and updates persist the main entity data and then use the same em.assign() code to ensure all relationship changes are put in place.

--- lib/data/src/databaseConnection.ts
+++ lib/data/src/databaseConnection.ts
@@ -73,7 +73,8 @@ export class DatabaseConnection<T extends BaseModel<any>> implements IDatastoreP
         // Get entity metadata
         const meta = this.em.getMetadata().get(data.constructor.name);
 
-        // Create a shallow copy of data, excluding m:n fields
+        // Create a shallow copy of data, excluding m:n fields, these are ignored for the
+        // shallow clone because those aren't stored in the row itself but rather a linking table
         const shallowData: Partial<T> = {};
         Object.keys(data).forEach((key) => {
             const property = meta.properties[key];
@@ -89,17 +90,19 @@ export class DatabaseConnection<T extends BaseModel<any>> implements IDatastoreP
         } else {
             // Fetch and merge for existing entity
             entity = await this.em.findOneOrFail(data.constructor.name, data.id);
-
-            this.immutableFields.forEach(f => {
-                if (f in data) {
-                    data[f] = entity[f];
-                }
-            });
-            // Use em.assign to properly handle m:n relationships for both new and existing entities
-            this.em.assign(entity, data);
-
-            await this.em.persist(entity).flush();
         }
+        // NOTE: this is a current hacky workaround to prevent the em.assign call trying to reset the entity's id
+        // to undefined on an insert. This should probably actually reference the schema metadata to determine the PK
+        delete data.id;
+
+        this.immutableFields.forEach(f => {
+            if (f in data) {
+                data[f] = entity[f];
+            }
+        });
+        // Use em.assign to properly handle m:n relationships for both new and existing entities
+        this.em.assign(entity, data);
+        await this.em.persist(entity).flush();
 
         return entity;
     }

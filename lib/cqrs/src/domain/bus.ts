2075e5daa443695b1a286dde9f7025d2de53832b
Robby Emmert <robby@emmert.io>
2024-01-01 23:08:19 -0500

    Adding types for an event bus system. Includes components: Events, Buses, and Event Processors. Events are meant to stand alone. Buses and Processors can be configured together to separate event fulfillment logic from event dispatching.

---
+++ lib/cqrs/src/domain/bus.ts
@@ -0,0 +1,40 @@
+import type { EventRef, EventRefName, IEvent } from './event'
+import type {
+    EventKey,
+    EventProcessorMap,
+    IEventProcessor,
+    SelectProcessorEvent,
+    UnwrapProcessorEvent,
+} from './event-processor'
+import type { PromiseOrValue } from '@declaro/core/src/typescript'
+
+export type BeforeEventCallback<E extends IEvent> = (
+    event: E,
+) => PromiseOrValue<boolean | void>
+export type AfterEventCallback<
+    E extends IEvent,
+    T extends EventProcessorMap,
+> = (event: E, result: DispatchResult<E, T>) => PromiseOrValue<boolean | void>
+
+export type DispatchResult<
+    E extends IEvent,
+    P extends EventProcessorMap,
+> = ReturnType<P[E['$name']]['process']>
+export interface IBus<T extends EventProcessorMap> {
+    dispatch<E extends IEvent>(event: E): Promise<DispatchResult<E, T>>
+    addEventProcessor<Proc extends IEventProcessor>(
+        processor: Proc,
+    ): IBus<
+        T & {
+            [key in Proc['event']]: Proc
+        }
+    >
+    before<E extends EventKey<T>>(
+        event: EventRef<E>,
+        callback: BeforeEventCallback<SelectProcessorEvent<T, E>>,
+    ): IBus<T>
+    after<E extends EventKey<T>>(
+        event: EventRef<E>,
+        callback: AfterEventCallback<SelectProcessorEvent<T, E>, T>,
+    ): IBus<T>
+}

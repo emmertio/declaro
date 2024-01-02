49561b309fadc428f9d6cc57bb82c8064b7b7076
Robby Emmert <robby@emmert.io>
2024-01-01 23:06:38 -0500

    Stubbing out a package for building CQRS applications.

---
+++ lib/cqrs/vite.config.ts
@@ -0,0 +1,24 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vitest/config'
+import dts from 'vite-plugin-dts'
+
+const extensions = {
+    es: 'mjs',
+    cjs: 'cjs',
+}
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'Declaro',
+            formats: ['es', 'cjs'],
+            fileName: (format) => `pkg.${extensions[format]}`,
+        },
+        write: true,
+    },
+    plugins: [dts()],
+    optimizeDeps: {
+        include: ['src/**/*'],
+    },
+})

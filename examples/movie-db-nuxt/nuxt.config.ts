933da87964d635a05fc131008cd70fe30d1f8636
Runner <support@emmert.io>
2024-05-31 17:25:13 -0400

    Setting up api within nuxt

--- examples/movie-db-nuxt/nuxt.config.ts
+++ examples/movie-db-nuxt/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: { enabled: true },
+    serverHandlers: [
+        {
+            route: '/api',
+            handler: '~/server/index.ts',
+            middleware: true,
+        },
+    ],
 })

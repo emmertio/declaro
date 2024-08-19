fe9c022cd0ea797b2feaacfd28c3fa83ba86d591
Runner <support@emmert.io>
2024-08-19 19:26:26 -0400

    Action code cleanup

--- lib/core/src/pipelines/pipeline-action.test.ts
+++ lib/core/src/pipelines/pipeline-action.test.ts
@@ -1,14 +1,14 @@
 import { describe, expect, it } from 'vitest'
-import { pipelineAction } from './pipeline-action'
 import { Pipeline } from './pipeline'
+import { pipelineAction } from './pipeline-action'
 
 describe('Pipeline Actions', () => {
-    it('should define pipeline actions', async () => {
-        const add = (increment: number) => pipelineAction((input: number) => input + increment)
-        const multiply = (factor: number) => pipelineAction((input: number) => input * factor)
-        const divide = (divisor: number) => pipelineAction((input: number) => input / divisor)
-        const formatResponse = async (input: number) => JSON.stringify({ result: input })
+    const add = (increment: number) => pipelineAction((input: number) => input + increment)
+    const multiply = (factor: number) => pipelineAction((input: number) => input * factor)
+    const divide = (divisor: number) => pipelineAction((input: number) => input / divisor)
+    const formatResponse = async (input: number) => JSON.stringify({ result: input })
 
+    it('should define pipeline actions', async () => {
         const pipeline = new Pipeline(add(1)).pipe(multiply(8)).pipe(divide(4)).pipe(formatResponse)
 
         const result = await pipeline.execute(1)

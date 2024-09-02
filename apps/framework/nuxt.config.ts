39e13c31fc430f8bb65e1a3f6ebc157731d135fa
Runner <support@emmert.io>
2024-09-01 23:25:07 -0400

    fixing layer import

--- apps/framework/nuxt.config.ts
+++ apps/framework/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    extends: ['../core', '../admin', '../schema'],
+    extends: ['./layers/core', './layers/admin', './layers/schema'],
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
     modules: ['@nuxtjs/tailwindcss', '@nuxtjs/storybook'],

13bab56462f7e62826d253859d346cb39f6e94e4
Runner <support@emmert.io>
2024-09-02 03:49:39 -0400

    Adding icon resources and pinia, to support menus infrastructure

--- apps/framework/nuxt.config.ts
+++ apps/framework/nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
     extends: ['./layers/core', './layers/admin', './layers/schema'],
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
-    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/storybook'],
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/storybook', '@pinia/nuxt', '@nuxt/icon'],
     tailwindcss: {
         cssPath: '~/assets/css/tailwind.css',
         configPath: 'tailwind.config.js',

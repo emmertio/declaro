5e2607c71ff43ac5b345a510c89dbfc7186b0ad8
Runner <support@emmert.io>
2024-11-03 03:27:19 -0500

    Removing the old core package in favor if one integrated framework nuxt project

--- apps/core/server/application/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-export function createEntityRouter() {
-    const router = Router()
-
-    // Routes
-
-    return router
-}
--- apps/core/server/infrastructure/drizzle.ts
+++
@@ -1,7 +0,0 @@
-import { DrizzleConfig } from 'drizzle-orm'
-import { DbConnection } from '../domain/interfaces/db'
-import { drizzle } from 'drizzle-orm/postgres-js'
-
-export async function createDrizzle(connection: DbConnection) {
-    return drizzle(connection)
-}

a00b0caa04954cd7905e82919f8ff366bb4829f9
Runner <support@emmert.io>
2025-04-17 14:20:14 -0400

    Updating event manager to support multiple event types

--- lib/core/src/events/event-manager.spec.ts
+++ lib/core/src/events/event-manager.spec.ts
@@ -5,7 +5,7 @@ class TestEvent {}
 
 describe('Event manager', () => {
     it('should add listeners', async () => {
-        const eventManager = new EventManager<TestEvent>()
+        const eventManager = new EventManager()
         const testEvent = new TestEvent()
 
         const mockListener = vi.fn((event: TestEvent) => {
@@ -20,7 +20,7 @@ describe('Event manager', () => {
     })
 
     it('should remove listeners', async () => {
-        const eventManager = new EventManager<TestEvent>()
+        const eventManager = new EventManager()
         const testEvent = new TestEvent()
 
         const cb = (event: TestEvent) => {
@@ -48,5 +48,30 @@ describe('Event manager', () => {
         expect(listener3.mock.calls.length).toBe(0)
         expect(listener4.mock.calls.length).toBe(1)
         expect(listener5.mock.calls.length).toBe(1)
+    })
+
+    it('should support listening to multiple events', async () => {
+        const eventManager = new EventManager()
+        const testEvent = new TestEvent()
+
+        const cb = (event: TestEvent) => {
+            expect(event).toBe(testEvent)
+        }
+
+        const listener1 = vi.fn(cb)
+        const listener2 = vi.fn(cb)
+
+        eventManager.on(['event-1', 'event-2'], listener1)
+        eventManager.on(['event-3', 'event-4', 'event-5'], listener2)
+
+        await eventManager.emitAll('event-1', testEvent)
+        await eventManager.emitAll('event-2', testEvent)
+        await eventManager.emitAll('event-3', testEvent)
+        await eventManager.emitAll('event-4', testEvent)
+        await eventManager.emitAll('event-5', testEvent)
+
+        expect(listener1.mock.calls.length).toBe(2)
+        expect(listener2.mock.calls.length).toBe(3)
+        expect(eventManager.getEvents().length).toBe(5)
     })
 })
--- lib/core/src/events/event-manager.ts
+++ lib/core/src/events/event-manager.ts
@@ -1,29 +1,40 @@
 export type Listener = (...args: any[]) => any
 
-export class EventManager<T> {
+export class EventManager {
     protected readonly listeners: {
         [key: string]: Listener[]
     } = {}
 
     getListeners(event: string) {
-        this.listeners[event] = Array.isArray(this.listeners[event])
-            ? this.listeners[event]
-            : []
+        this.listeners[event] = Array.isArray(this.listeners[event]) ? this.listeners[event] : []
         return this.listeners[event]
     }
 
-    on(event: string, listener: Listener) {
+    getEvents() {
+        return Object.keys(this.listeners)
+    }
+
+    on(event: string | string[], listener: Listener) {
+        if (Array.isArray(event)) {
+            const removeListeners = event.map((e) => {
+                this.getListeners(e).push(listener)
+                return () => {
+                    const index = this.getListeners(e).indexOf(listener)
+                    if (index > -1) {
+                        this.getListeners(e).splice(index, 1)
+                    }
+                }
+            })
+            return () => removeListeners.forEach((remove) => remove())
+        }
+
         this.getListeners(event).push(listener)
 
         return () => {
             const index = this.getListeners(event).indexOf(listener)
-            const willRemove = index > -1
-
-            if (willRemove) {
+            if (index > -1) {
                 this.getListeners(event).splice(index, 1)
             }
-
-            return willRemove
         }
     }
 
@@ -39,9 +50,7 @@ export class EventManager<T> {
     }
 
     async emitAll(event: string, ...args: any[]) {
-        await Promise.all(
-            this.getListeners(event).map((listener) => listener(...args)),
-        )
+        await Promise.all(this.getListeners(event).map((listener) => listener(...args)))
     }
 
     emit(event: string, ...args: any[]) {

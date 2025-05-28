3c7556cf6f6d31bc0104e0a986e9ab3b13167c33
Runner <support@emmert.io>
2025-05-28 18:49:43 -0400

    Add @declaro/data library with initial setup and configuration files

---
+++ lib/data/scripts/build.ts
@@ -0,0 +1,31 @@
+import { build } from 'bun'
+import { resolve } from 'path'
+
+const defaults = {
+    entrypoints: [resolve(__dirname, '../src/index.ts')],
+}
+
+await Promise.all([
+    build({
+        ...defaults,
+        target: 'node',
+        format: 'cjs',
+        splitting: true,
+        outdir: 'dist/node',
+        sourcemap: 'linked',
+    }),
+    build({
+        ...defaults,
+        target: 'browser',
+        outdir: 'dist/browser',
+        sourcemap: 'linked',
+        minify: true,
+    }),
+    build({
+        ...defaults,
+        target: 'bun',
+        splitting: true,
+        outdir: 'dist/bun',
+        sourcemap: 'linked',
+    }),
+])

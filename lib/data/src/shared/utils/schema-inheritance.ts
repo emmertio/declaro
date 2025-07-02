4a8f62138ea47c44b6908018c76ce16bd9d889ef
Runner <support@emmert.io>
2025-07-02 10:51:18 -0400

    chore: update package versions to 2.0.0-beta.46 and add schema-inheritance utility type

---
+++ lib/data/src/shared/utils/schema-inheritance.ts
@@ -0,0 +1,26 @@
+import type { Model, ModelSchema } from '@declaro/core'
+
+/**
+ * Represents a child schema that inherits from a parent schema.
+ * This is useful for creating schemas that extend the functionality of an existing schema.
+ * It replaces the schema name and all model names with a string type.
+ *
+ * @warning This type is intended for use in generic types. In most cases, you should use concrete schemas for inheritance for best type inference.
+ *
+ * @template TSchema - The parent schema type.
+ * @example
+ * ```ts
+ * import { ModelSchema, ChildSchema } from '@declaro/core';
+ *
+ * export class ParentService<TSchema extends ChildSchema<typeof ParentSchema>> extends ModelService<TSchema> {
+ *     constructor(args: IModelServiceArgs<TSchema>) {
+ *         super(args);
+ *     }
+ * }
+ */
+export type ChildSchema<TSchema extends ModelSchema> = ModelSchema<
+    string,
+    {
+        [K in keyof TSchema['definition']]: Model<string, TSchema['definition'][K]['schema']>
+    }
+>

9c5245df55a86310afa69471a9abca34fa596b7b
Runner <support@emmert.io>
2025-10-09 16:36:19 -0400

    refactor: clean up browser-specific exports in index.browser.ts

--- lib/auth/src/index.browser.ts
+++ lib/auth/src/index.browser.ts
@@ -5,6 +5,3 @@
 
 export * from './domain/models/auth-session'
 export * from './domain/interfaces/auth-config'
-export * from './shared/utils/auth-validator'
-export * from './shared/decorators/validate-permissions'
-export * from './types/auth-context'
